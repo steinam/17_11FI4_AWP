875dafbd6d18570168acb539bf895bcfcbf79e4d
Karl Steinam <webmaster@steinam.de>
2018-01-24 22:28:09 +0100

    Ausgangsmaterial für Beispiele testverfahren (NUnit, TDD, White-, Blackbox)

---
+++ woche_3/Stacker/Stacker/myStack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stacker
+{
+    public class myStack
+    {
+
+
+        public bool isEmpty()
+        {
+            int a = 5;
+            if (a < 5)
+            {
+
+                return true;
+            }
+            else
+            {
+                return false;
+                    
+            }
+        }
+
+
+    }
+}
---
+++ woche_3/Stacker/TestStack/TestStacker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Stacker;
+
+namespace TestStack
+{
+
+    [TestFixture]
+    class TestStacker
+    {
+
+
+
+        [Test]
+        public void testEnsureObjectBeforePushIsNotEmpty()
+        {
+
+     
+
+        }
+
+
+
+    }
+}
